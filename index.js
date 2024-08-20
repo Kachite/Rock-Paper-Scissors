7d7638e60ec0ded9e3e709d47ecb8d599e99775c
Uzomba Onyekachi Victor <victor.uzomba@yahoo.com>
2024-08-20 05:21:59 +0300

    Loop the game for multiple inputs

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// console.log("Hello World!");
 function getComputerChoice() {
   let options = ["rock", "paper", "scissors"];
   let computerChoice = options[Math.floor(Math.random() * options.length)];
@@ -14,27 +13,37 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    console.log("It's a draw!");
-  } else if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "scissors" && computerChoice === "paper") ||
-    (humanChoice === "paper" && computerChoice === "rock")
-  ) {
-    console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-    humanScore++;
-  } else {
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-    computerScore++;
-  }
 
-  console.log(`Human Score: ${humanScore} | Computer Score: ${computerScore}.`);
+function playGame (){
+    for(let i = 0; i <=5 ; i++){
+        function playRound(humanChoice, computerChoice) {
+            if (humanChoice === computerChoice) {
+              console.log("It's a draw!");
+            } else if (
+              (humanChoice === "rock" && computerChoice === "scissors") ||
+              (humanChoice === "scissors" && computerChoice === "paper") ||
+              (humanChoice === "paper" && computerChoice === "rock")
+            ) {
+              console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+              humanScore++;
+            } else {
+              console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+              computerScore++;
+            }
+          
+            console.log(`Human Score: ${humanScore} | Computer Score: ${computerScore}.`);
+          }
+          
+          
+          const computerSelction = getComputerChoice();
+          const humanSelection = getHumanChoice();
+          
+          console.log("Computer Selection:", computerSelction);
+          console.log("Human Selection:" ,humanSelection);
+          
+          playRound(humanSelection, computerSelction);
+    }
+      
 }
 
-console.log(computerSelction);
-console.log(humanSelection);
-const computerSelction = getComputerChoice();
-const humanSelection = getHumanChoice();
-
-playRound(humanSelection, computerSelction);
+playGame();
